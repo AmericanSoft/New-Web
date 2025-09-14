7ebf0f93bd23ab1d1b07b00fd4066b2bdf615152
mostafahp <mostafahp44@gmail.com>
2025-09-15 00:52:05 +0300

    sec

---
+++ src/pages/ArticalsPages.tsx
@@ -0,0 +1,15 @@
+
+
+
+const articalblog =()=>{
+return(
+   <>
+   <div className="container grid">
+    <div className="text-red-500"></div>
+    <div></div>
+    <div></div>
+   </div>
+  </>
+)
+}
+export default articalblog
